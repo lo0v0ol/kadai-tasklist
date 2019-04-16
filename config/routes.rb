c5706d94ab3e6d7462488a7f3b387a5e6973080d
aya <aya.program.use@gmail.com>
2019-04-16 02:38:02 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'user#destroy'
+ 
+ get 'signup', to: 'tasks#new'
  resources :tasks
 end
