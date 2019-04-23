de5660f32571b81bf9e2ba03f8bc9e70c254fa6e
aya <aya.program.use@gmail.com>
2019-04-23 14:14:14 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+    
   def index
-    @tasks = Task.all.page(params[:page])
+   if logged_in?
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+   end
   end
 
   def show
@@ -12,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
@@ -40,14 +45,19 @@ class TasksController < ApplicationController
   end
  
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
   
   private
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
   
   def task_params
     params.require(:task).permit(:content, :status)
